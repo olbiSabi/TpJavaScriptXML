a1ec213aa1a3acc69497666de3d1f4830a0beda5
olbi <rabixx25@gmail.com>
2021-02-10 02:32:38 +0100

    Bonus changement dynamique du temps en fonction de la ville

--- meteo.js
+++ meteo.js
@@ -1,19 +1,30 @@
+let apiCall = (city) => {
+    const apiKey = "bd6b3aa0299986e1043f0b7b23e81ec4";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`;
 
-function showCity(){
-    
+    fetch(url).then((response) =>
+            response.json().then((data) => {
+                console.log(data)
+                document.querySelector('.temps' + city).innerHTML = data.main.temp;
+            })
+        )
+        .catch((err) => console.log('Erreur :' + err));
+}
+
+function showCity() {
+
     let message = document.querySelector('#label');
     let nomVilleSelectionnee = document.getElementById('ville').value;
+    apiCall(nomVilleSelectionnee);
     let villes = document.getElementsByClassName('city');
-    let taille = villes.length;
-    for(let i = 0; i < taille; i++){
-        if(nomVilleSelectionnee == villes[i].id){
-            villes[i].style.display="";
+    for (let i = 0; i < villes.length; i++) {
+        if (nomVilleSelectionnee == villes[i].id) {
+            villes[i].style.display = "";
             villes[i].style.width = "100%";
-            message.firstChild.nodeValue = "You selected: "+villes[i].id;
+            message.firstChild.nodeValue = "You selected: " + villes[i].id;
 
+        } else {
+            villes[i].style.display = "none";
         }
-        else{
-            villes[i].style.display= "none";
-        }
-    } 
+    }
 }
